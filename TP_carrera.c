3430a860963db491b9536c2845a042ee7c058acf
Cesar Flores <68258111+cesoss@users.noreply.github.com>
2022-11-20 18:42:02 -0300

    Entrega de TP

--- TP_carrera.c
+++ TP_carrera.c
@@ -1,7 +1,5 @@
 #include"carrera.h"
-void lista_corredores(void);
-//void ver_listado(datos_t* datos);
-void imprimir_menu(void);
+
 
 
 
@@ -27,41 +25,83 @@ void imprimir_menu(void){
 
 
 }
+//ingreso de los datos de los competidores
+datos_t* agregar_competidor(void){
 
-datos_t* agregar_competidor(){
+    short opc;
+
+    cola_t *cola=NULL, *aux=NULL,*local=NULL;
 
     datos_t* ingresar=malloc(sizeof(datos_t));
     if(!ingresar)printf("ERROR de memoria");
     fflush(stdin);
+    do{
+        aux=(cola_t*)malloc(sizeof(cola_t));
+        if(!aux)printf("ERROR de cola");
 
-    printf("-----INGRESAR DATOS DE COMPETIDOR/A----\n\n");
-    printf("Nombre y Apellido: ");
-    gets(ingresar->nombre);
-    printf("Edad: ");
-    scanf("%d",&ingresar->edad);
-    printf("Categoria: ");
-    scanf("%c%d",&ingresar->c,&ingresar->categoria);
-    printf("Ingrese DNI: ");
-    scanf("%d",&ingresar->dni);
-    printf("Numero de participante: ");
-    scanf("%d",&ingresar->numero_c);
-    free(ingresar);
+
+        printf("-----INGRESAR DATOS DE COMPETIDOR/A----\n\n");
+        printf("Nombre y Apellido: ");
+        gets(aux->datos.nombre);
+        printf("Edad: ");
+        scanf("%d",&aux->datos.edad);
+        printf("Categoria: ");
+        scanf("%c%d",&aux->datos.categoria);
+        printf("Ingrese DNI: ");
+        scanf("%d",&aux->datos.dni);
+        printf("Numero de participante: ");
+        scanf("%d",&aux->datos.numero_c);
+
+        aux->siguiente=NULL;
+        if(cola==NULL){
+            cola=aux;
+            local=aux;
+        }else{
+            local->siguiente=aux;
+            local=aux;
+        }
+        printf("\nIngresa a otro Competidor? (si=1, no=0)");
+        scanf("%d",&opc);
+        
+
+
+
+
+        free(ingresar);
+        free(aux);
+
+    }while(opc==1);
+
     return ingresar;
 }
-void ver_listado(datos_t* datos){
 
-    printf("-------- Listado Ingresado--------\n\n");
-    fflush(stdin);
-    printf("Nombre: %s , #%.2d ,edad %d , cat %d, DNI: %d",datos->nombre,datos->numero_c,datos->edad,datos->categoria,datos->dni);
+//arma una lista con los competidores, a medida que se cargan
+void ver_listado(datos_t *ingresar ){
+    cola_t *cola=NULL,*aux=NULL,*local=NULL;
+    do{
+
+        printf("-------------------- Listado Ingresado de COMPETIDORES-----------------------------\n\n");
+        fflush(stdin);
+
+        printf("Nombre: %s , #%.2d ,edad %d , cat %d, DNI: %d",cola->datos.nombre,cola->datos.numero_c,cola->datos.edad,cola->datos.categoria,cola->datos.dni);
+        aux=cola;
+        cola=cola->siguiente;
+    }while(cola->siguiente=NULL);
+    printf("------------------------------------------------------------------------------------------");
+    printf("Nombre: %s , #%.2d ,edad %d , cat %d, DNI: %d",cola->datos.nombre,cola->datos.numero_c,cola->datos.edad,cola->datos.categoria,cola->datos.dni);
+    printf("----------------------------------FIN DE LISTA--------------------------------------------");
+    free(aux);
 
 
 
 }
+
+//se copian los datos en un archivo
 void copiar_archivo(void){
     FILE* listado_carrera;
     datos_t* datos_t;
 
-    if((listado_carrera=fopen("listado_carrera.txt","rt+"))==NULL)printf("ERROR de archivo");
+    if((listado_carrera=fopen("listado_carrera.txt","at+"))==NULL)printf("ERROR de archivo");
     if(fwrite(&datos_t,sizeof(datos_t),1,listado_carrera))printf("ERROR no se pudo copiar datos");
 
     fclose(listado_carrera);
@@ -75,6 +115,8 @@ void corregir_datos(){
     }
 
    }
+
+
 int iniciar_carrera(int s,int m,int h ){
                 long tiempo;
                 int inicio=0;
@@ -88,10 +130,10 @@ int iniciar_carrera(int s,int m,int h ){
 
     return tiempo;
 }
+//menu del programa
 
-
-int main(){
-    int o,salir=0, tiempo;
+int menu(long tiempo){
+    int o,salir=0;
     int s=0,m=0,h=0;
     datos_t* lista;
 
@@ -107,13 +149,108 @@ int main(){
             if(o==2)ver_listado(lista);
             if(o==3)corregir_datos();
             if(o==4)copiar_archivo();
-            if(o==5)iniciar_carrera();
+            if(o==5)iniciar_carrera(s,m,h);
             if(o==6)salir=1;
 
     }
 
 
 
+
+    return 0;
+}
+//comienzo de la carrera con el cronometro 
+int crono_carrera(int seg, int minu, int hora){
+    seg=0,minu=0,hora=0;
+    datos_t tiempo;
+    long cantidad;
+    datos_t *corredor;
+    corredor=(datos_t*)malloc(sizeof(datos_t));
+    if(!corredor) printf("ERROR de memoria");
+    cantidad=corredor->numero_c;
+
+    printf("los corredores son:  %d\n\n",corredor->numero_c);
+    do{
+            tiempo.tiempo=cronometro_h(seg,minu,hora);
+
+            printf("\r%.2d:%.2d:%.2d",hora,minu,seg);
+
+            cantidad++;
+            printf("\n\ncantidad de corredores que pasaron son:  %d\n\n",cantidad);
+    }while(cantidad>1);
+    printf("\nfuera de carrera....");
+
+
+    return 0;
+}
+//funcion de largada de la carrera
+int comienzo_carrera(long cant){
+    datos_t tiempo;
+    int s, m ,h;
+    cant=tiempo.numero_c;
+
+    while(cant>1){
+        tiempo.tiempo=crono_carrera(s,m,h);
+
+
+    }
+    printf("----------------No inicio competencia!!!!!!!!!!!!!!!!!!!!!-------------------");
+
+
+
+
+    return 0;
+}
+//main general del programa
+int main(){
+    int a=0,b=0,c=0;
+    long tiempo;
+    long cant, estado=0; 
+    
+    datos_t cantn;
+    
+
+    menu(tiempo);
+    cant=cantn.numero_c;
+
+    printf("lectura del sensor: 1:\n 2;\n 3:\n 4:\n 5:\n ");
+    scanf("%d",&estado);
+    while(cant>1){
+        switch (estado)
+        {
+        case 1:
+            crono_carrera(a,b,c);//empieza a correr el cronometro
+            comienzo_carrera(cant);//cuenta la cantidad de participantes
+            printf("los participantes son: %d\n",cant);//muestra los participantes
+            break;
+        case 2:
+            printf("----------Pasaron por el segundo totem------\n");//muestran la lectura de los sensores
+            break;
+        case 3:
+            printf("Los tiempos parciales son: %ld",tiempo);//muestra los tiempos parciales
+            break;
+        case 4:
+            printf("-------lugar donde se encuentran los participantes segun lectura del sensor:     -----------");//muestra ubicacion de los participantes en la carrera
+            break;
+        case 5:
+            printf("------------FINAL de carrera------------");
+            printf(" resultados finales: %ld, %d",tiempo,cantn );//muestra los datos de la carrera
+            break;
+        default: printfg("ERROR de programa");     
+        }
+
+
+
+
+    }
+    printf("\n\n-------------------------------Gracias por usar CARCRO!!!!!!!-----------------------\n\n");
+
+    
+    
+    
+
+
+    
 
     return 0;
 }
--- carrera.h
+++ carrera.h
@@ -3,6 +3,16 @@
 #include<string.h>
 #include<windows.h>
 #include<malloc.h>
+#include<conio.h>
+void lista_corredores(void);
+//void ver_listado(datos_t* datos);
+void imprimir_menu(void);
+datos_t* agregar_competidor(void);
+void ver_listado(datos_t *ingresar );
+void copiar_archivo(void);
+int iniciar_carrera(int s,int m,int h );
+int menu(long tiempo);
+int crono_carrera(int,int,int);
 
 
 int cronometro_h(int,int,int);
@@ -18,6 +28,12 @@ typedef struct carga_datos{
     long tiempo;
 
 }datos_t;
+
+typedef struct cola{
+    datos_t datos;
+    struct cola_t*siguiente;
+
+}cola_t;
 
 
 int cronometro_h(int seg,int minu,int hora){
